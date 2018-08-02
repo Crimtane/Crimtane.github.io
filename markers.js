007e03b3acac98a1d9d27739bf1b55fe4a4fccc3
Crimtane <42019876+Crimtane@users.noreply.github.com>
2018-08-03 00:51:08 +0200

    Add files via upload

---
+++ markers.js
@@ -0,0 +1,25 @@
+var markers={
+  "normalrender": [
+    {
+      "groupName": "Base9a27_cc2f", 
+      "icon": "icons/marker_tower.png", 
+      "createInfoWindow": true, 
+      "displayName": "Base", 
+      "checked": false
+    }, 
+    {
+      "groupName": "Location18a5_cc2f", 
+      "icon": "icons/marker_location.png", 
+      "createInfoWindow": true, 
+      "displayName": "Location", 
+      "checked": false
+    }, 
+    {
+      "groupName": "Town18ac_cc2f", 
+      "icon": "icons/marker_town.png", 
+      "createInfoWindow": true, 
+      "displayName": "Town", 
+      "checked": false
+    }
+  ]
+};
---
+++ markersDB.js
@@ -0,0 +1,69 @@
+var markersDB={
+  "Base9a27_cc2f": {
+    "raw": [
+      {
+        "y": 64, 
+        "x": -1500, 
+        "hovertext": "Crimtane & olem50", 
+        "z": -8500, 
+        "text": "Crimtane & olem50's base of operation"
+      }, 
+      {
+        "y": 64, 
+        "x": 940, 
+        "hovertext": "Mewriel", 
+        "z": -6255, 
+        "text": "Mewriel's beach island"
+      }
+    ], 
+    "name": "Base", 
+    "created": false
+  }, 
+  "Town18ac_cc2f": {
+    "raw": [
+      {
+        "y": 64, 
+        "x": -2000, 
+        "hovertext": "Happy Mask Salesman", 
+        "z": -8090, 
+        "text": "The overcrowded area"
+      }, 
+      {
+        "y": 64, 
+        "x": -1960, 
+        "hovertext": "Weenston", 
+        "z": -8450, 
+        "text": "Weenston's town"
+      }, 
+      {
+        "y": 64, 
+        "x": -2250, 
+        "hovertext": "Vezur", 
+        "z": -8500, 
+        "text": "Vezur's town"
+      }
+    ], 
+    "name": "Town", 
+    "created": false
+  }, 
+  "Location18a5_cc2f": {
+    "raw": [
+      {
+        "y": 64, 
+        "x": -1580, 
+        "hovertext": "Slime Farm", 
+        "z": -8620, 
+        "text": "Crimtane's slime farm"
+      }, 
+      {
+        "y": 64, 
+        "x": -3800, 
+        "hovertext": "Stronghold", 
+        "z": -7785, 
+        "text": "Stronghold under this mountain"
+      }
+    ], 
+    "name": "Location", 
+    "created": false
+  }
+};
